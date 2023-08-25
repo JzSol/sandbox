f6d65d4f8ec20bb133c647031b1d737ad5dee465
Janis.Zurilo <janis.zurilo@veloceapps.com>
2023-08-25 09:47:05 +0300

    learning classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sandbox';
 }
+
+class Animal {
+  constructor(public name: string) {}
+
+  move(distance: number) {
+    console.log(`${this.name} moved ${distance} meters.`);
+  }
+}
+
+class Snake extends Animal {
+  slither(distance: number) {
+    console.log(`${this.name} is slithering...`);
+    super.move(distance);
+  }
+}
+new Snake('da');
